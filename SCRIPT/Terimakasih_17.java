99c41e1fbb99d16eb9b9188673bdda489dfab0f2
uhamhz <ammarhafizh2404@gmail.com>
2023-11-22 20:45:58 +0700

    feat : function with parameter

--- SCRIPT/Terimakasih_17.java
+++ SCRIPT/Terimakasih_17.java
@@ -5,8 +5,18 @@ package SCRIPT;
  */
 public class Terimakasih_17 {
 
+    public static void UcapanTerimakasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
     public static void main(String[] args) {
-         System.out.println("Thank you for being the best teacher in the world.\n" + 
-        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        UcapanTerimakasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
